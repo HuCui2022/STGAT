d457fc87ff526173a27ea0fbacb62d18ac07bf3f
HuCui2022 <39700284+HuCui2022@users.noreply.github.com>
2023-06-22 15:56:52 +0900

    Update st2ransformer_dsta.py

--- model/st2ransformer_dsta.py
+++ model/st2ransformer_dsta.py
@@ -222,9 +222,9 @@ class STAttentionBlock(nn.Module):
                 attention = F.softmax(attention, dim=-1)#* self.betas.repeat(1,1,self.num_node,1)
                 self.graph_a = self.graph_a.cuda(x.get_device())
                 diff = (self.diff_net(torch.einsum('nctu,uv->nctv',y, self.graph_a)).repeat(1,1,1,self.window_size) - upfold)\
-                    .view(N, self.num_subset, self.in_channels//self.num_subset, T ,self.window_size, V)  #nsctwv   根据距离进行分组，来减少计算。
+                    .view(N, self.num_subset, self.in_channels//self.num_subset, T ,self.window_size, V)  #nsctwv   计算差值。
                 diff = torch.sigmoid(diff.mean(-1).mean(2).mean(2)) # nsw
-                attention = attention *diff.unsqueeze(-1).repeat(1,1,self.num_node,1)
+                attention = attention *diff.unsqueeze(-1).repeat(1,1,self.num_node,1)  # nsw -> n 1 s*v w; attention shape : n s u v;
             if self.glo_reg_s:
                 attention = attention + self.attention0s.repeat(N, 1, 1, 1)
             attention = self.drop(attention)
